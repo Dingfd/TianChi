4f75790872203d4b220c4b930ccce102e55fe5be
D <1139993551@qq.com>
2019-03-15 23:23:20 +0800

    111

--- Coupon/decision_tree/tree.py
+++ Coupon/decision_tree/tree.py
@@ -3,3 +3,5 @@ import pandas as pd
 pd.set_option('display.max_columns', None)
 train_offline = pd.read_csv('../data/ccf_offline_stage1_train.csv')
 train_online = pd.read_csv('../data/ccf_online_stage1_train.csv')
+
+print('ddd')
