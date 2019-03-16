038f01a0ef86da97a5f676a7a1b4c03bbd89fd54
D <1139993551@qq.com>
2019-03-16 17:57:57 +0800

    3.15-work

--- Happiness/algorithm/tree.py
+++ Happiness/algorithm/tree.py
@@ -1,1 +1,17 @@
 import pandas as pd
+
+pd.set_option('display.max_columns', None)
+train_abbr = pd.read_csv('../data/happiness_train_abbr.csv')
+test_abbr = pd.read_csv('../data/happiness_test_abbr.csv')
+train_complete = pd.read_csv('../data/happiness_train_complete.csv')
+test_complete = pd.read_csv('../data/happiness_test_complete.csv')
+print('删减的训练集：')
+print(train_abbr.info())
+print('删减的测试集')
+print(test_abbr.info())
+print('完整的训练集：')
+print(train_complete.info())
+print('完整的测试集：')
+print(test_complete.info())
+print('train_abbr 5 rows:', train_abbr.sample(5))
+print('test_abbr 5 rows:', test_abbr.sample(5))
