2c00a7582494b188109640767972620e1e4ffacb
D <1139993551@qq.com>
2019-03-20 20:15:06 +0800

    3.20-work

--- Happiness/algorithm/tree.py
+++ Happiness/algorithm/tree.py
@@ -1,19 +1,25 @@
 import pandas as pd
+from sklearn import model_selection
 
 pd.set_option('display.max_columns', None)
 train_abbr = pd.read_csv('../data/happiness_train_abbr.csv')
 test_abbr = pd.read_csv('../data/happiness_test_abbr.csv')
-train_complete = pd.read_csv('../data/happiness_train_complete.csv', encoding='gbk')
-test_complete = pd.read_csv('../data/happiness_test_complete.csv', encoding='gbk')
-# print('删减的训练集：')
-# print(train_abbr.info())
-# print('删减的测试集')
-# print(test_abbr.info())
-# print('完整的训练集：')
-# print(train_complete.info())
-# print('完整的测试集：')
-# print(test_complete.info())
-# print('train_abbr 5 rows:', train_abbr.sample(5))
-# print('test_abbr 5 rows:', test_abbr.sample(5))
-print(train_abbr.loc[(train_abbr['survey_type'] == 2 )& (train_abbr['work_exper'] == 3)&(train_abbr['family_income']>0)]
-      ['family_income'].mean())
+# train_complete = pd.read_csv('../data/happiness_train_complete.csv', encoding='gbk')
+# test_complete = pd.read_csv('../data/happiness_test_complete.csv', encoding='gbk')
+
+#将null值设为农村样本的家庭收入的平均值
+train_abbr.loc[train_abbr['family_income'].isnull(),'family_income']\
+      = train_abbr.loc[(train_abbr['survey_type'] == 2 )&
+                       (train_abbr['work_exper'] == 3)&
+                       (train_abbr['family_income']>0)]['family_income'].mean()
+
+train_abbr_set = train_abbr.copy(deep=True)
+target = train_abbr_set["happiness"]
+train = train_abbr_set.drop(["happiness"], axis=1)
+# print('train_abbr columns:', train_abbr_set.columns.tolist())
+# print('train columns:', train.columns.tolist())
+# print('train set :',train.sample(10))
+# print('target set :',target.sample(10))
+train_x, test_x, train_y, test_y = model_selection.train_test_split(train, target,random_state=1)
+
+
